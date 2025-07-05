04f35c728e7f3f07422ea7384c174039d108713b
mrcmd29 <mendozajorge17@hotmail.com>
2025-07-05 17:38:39 -0400

    Inicio del trabajo
    Avance que hice desde el trabajo

---
+++ myweb/mainmain/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import views
+from django.urls import path
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.base, name='base.html'),
+   
+]
