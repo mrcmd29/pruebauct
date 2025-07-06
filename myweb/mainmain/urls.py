798bb933695acd19a791d8c2e93d02324baf2542
mrcmd29 <mendozajorge17@hotmail.com>
2025-07-05 23:50:31 -0400

    "cansado"

--- myweb/mainmain/urls.py
+++ myweb/mainmain/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.base, name='base.html'),
-   
+    path('main/', views.main, name='main.html'),   
+    path('agenda/', views.agenda, name='agenda.html'),
 ]
--- myweb/mainmain/views.py
+++ myweb/mainmain/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 def base(request):
     return render(request, 'base.html')
+def main(request):
+    return render(request, 'main.html')
+def agenda(request):
+    return render(request, 'agenda.html'    )
 
 # Create your views here.
